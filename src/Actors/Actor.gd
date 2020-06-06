dddb585711744530246142778e6be40b5429429b
Jai <jaikuraria@gmail.com>
2020-06-06 09:11:00 +0530

    First commit

---
+++ src/Actors/Actor.gd
@@ -0,0 +1,9 @@
+extends KinematicBody2D
+class_name Actor
+
+export var gravity = 3000
+export var velocity = Vector2.ZERO
+
+func _physics_process(delta: float) -> void:
+	velocity.y += gravity * delta
+	move_and_slide(velocity)
